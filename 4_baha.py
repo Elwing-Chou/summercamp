9a1d07d3385f3c280047c973adea824946ad6af6
Elwing-Chou <birdfan8814@gmail.com>
2025-08-20 10:09:05 +0800

    Add files via upload

---
+++ 4_baha.py
@@ -0,0 +1,21 @@
+scores = [30, 20, 60, 80]
+print(scores[0])
+print(len(scores))
+# 反向編號: -4 -3 -2 -1
+print(scores[-1])
+# 走過
+for s in scores:
+    print("!", s)
+
+# 字典
+student = {
+    "height":175,
+    "age":18
+}
+print(student["age"])
+
+
+
+
+
+
