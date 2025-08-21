1c7e3fde2114fa8fc18832f01984506e4ad77f3c
Elwing-Chou <birdfan8814@gmail.com>
2025-08-21 10:17:23 +0800

    Add files via upload

---
+++ 9_五子棋.py
@@ -0,0 +1,22 @@
+import pygame as pg
+#pygame初始化
+pg.init()
+
+#設定視窗
+width, height = 800, 800
+screen = pg.display.set_mode((width, height))
+pg.display.set_caption("五子棋")
+
+#建立畫布bg
+bg = pg.Surface(screen.get_size())
+bg = bg.convert()
+bg.fill((199, 167, 82))
+pg.display.update()
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+pg.quit()
