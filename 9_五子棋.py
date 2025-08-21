44e1bf75b4d9cf5446f4b6305ca075ca56c3fb6c
Elwing-Chou <birdfan8814@gmail.com>
2025-08-21 10:28:31 +0800

    Add files via upload

--- 9_五子棋.py
+++ 9_五子棋.py
@@ -3,21 +3,29 @@ import pygame as pg
 pg.init()
 
 #設定視窗
-width, height = 800, 800
+width, height = 600, 600
+# 產生視窗
 screen = pg.display.set_mode((width, height))
+# 設定遊戲標題
 pg.display.set_caption("五子棋")
 
-#建立畫布bg
+# 建立畫布bg
 bg = pg.Surface(screen.get_size())
-bg = bg.convert()
+# 把畫布填滿某個顏色
 bg.fill((199, 167, 82))
+# 把某個圖層貼到上一層
 screen.blit(bg, (0,0))
+# 對畫面進行更新(才會真的秀出來)
 pg.display.update()
 
+# 建立一個永不結束的迴圈(遊戲才不會結束)
 running = True
 while running:
+    # 收取你的遊戲任何事件(滑鼠點擊/鍵盤按鈕...)
     for event in pg.event.get():
+        # 如果收到的事件是按x
         if event.type == pg.QUIT:
+            # 迴圈就會變成while False
             running = False
-
+# 結束遊戲
 pg.quit()
